32cc94ed6ccacfcc8e6525a1e5d7e2acceefb7fe
Manuel Suarez <manuelsuarezpena1993@gmail.com>
2022-08-01 20:34:59 -0300

    Added health bar

--- Assets/Scripts/General/Health.cs
+++ Assets/Scripts/General/Health.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] int _maxHealth;
+    [SerializeField] int _maxHealth; 
+    [SerializeField] Image _img;
     private int _currentHealth;
-
+    private void Update()
+    {
+        if (_img != null)
+            _img.fillAmount = (float)_currentHealth / _maxHealth;
+    }
     public int CurrentHealth => _currentHealth;
-    private void Start() => _currentHealth = _maxHealth;
-
+    private void Start() => _currentHealth = _maxHealth; 
     public void ChangeHealth(int health) => _currentHealth = health;
     public void RemoveHealth(int health) => _currentHealth -= health;
     public void AddHealth(int health) => _currentHealth += health;
